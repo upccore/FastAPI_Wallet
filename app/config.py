12781b594cd38b0bb6d8b5581ac855fe259d2fcc
Denis Smirnov <upccore@list.ru>
2026-05-07 12:23:51 +0300

    .gitignore, .env.template, config.py added

---
+++ app/config.py
@@ -0,0 +1,9 @@
+import os
+
+POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "wallets_db")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+
+DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
