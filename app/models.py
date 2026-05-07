20c05e82c510a2bd8803ddcd3af278b3a4f7cf7e
Denis Smirnov <upccore@list.ru>
2026-05-07 12:29:01 +0300

    models.py added

---
+++ app/models.py
@@ -0,0 +1,11 @@
+import uuid
+from sqlalchemy import Column, Numeric
+from sqlalchemy.dialects.postgresql import UUID
+from .database import Base
+
+
+class Wallet(Base):
+    __tablename__ = "wallets"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    balance = Column(Numeric(10, 2), default=0.00, nullable=False)
